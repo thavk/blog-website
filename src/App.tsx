7b4d7c6356c7e05f2790ca1814dab5831defcac1
thavk <marco_estela_ferreira@hotmail.com>
2025-08-11 12:45:16 +0100

    [UI+Logic] Created base login page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login-page';
 
 function App() {
-  const [message, setMessage] = useState('Loading...');
+    const [message, setMessage] = useState('Loading...');
 
-  useEffect(() => {
-    fetch('http://localhost:3001')
-      .then(res => res.text())
-      .then(setMessage)
-      .catch(() => setMessage('Failed to connect to backend.'));
-  }, []);
+    useEffect(() => {
+        fetch('http://localhost:5432')
+            .then(res => res.text())
+            .then(setMessage)
+            .catch(() => setMessage('Failed to connect to backend.'));
+    }, []);
 
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Backend Response</h1>
-      <p>{message}</p>
-    </div>
-  );
+    return (
+        <CssVarsProvider>
+            <Sheet style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<h1>{message}</h1>}/>
+                        <Route path='/login' element={<LoginPage/>}/>
+                    </Routes>
+                </Router>
+            </Sheet>
+        </CssVarsProvider>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <CssVarsProvider>
+                <App />
+            </CssVarsProvider>
+        </StyledEngineProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
